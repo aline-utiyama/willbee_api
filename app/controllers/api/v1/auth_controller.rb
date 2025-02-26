80e5731604c4d03ca1885377bfdc7eda83c54610
aline-utiyama <lineskutiyama@gmail.com>
2025-02-26 13:33:00 +0900

    Turn sidekiq schedule to every hour

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::AuthController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id, email: @user.email)
+      SendReminderNotificationJob.perform_now
       render json: { token:, user: { id: @user.id, email: @user.email, name: @user.name } }, status: :ok
     else
       render json: { error: 'Invalid email or password' }, status: :unauthorized
